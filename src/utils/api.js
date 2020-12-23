a3e6ba236bb79e2c55b2e32dfde2b16e13cd9400
t36campbell <campbell3636@yahoo.com>
2020-12-22 19:08:50 -0500

    updated api url for functions and tested deployed app

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: `http://localhost:8888/.netlify/functions/`
+  baseURL: `https://react-itinerary.netlify.app/.netlify/functions/`
 });
